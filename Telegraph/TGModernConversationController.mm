1260a07b46e40a8d322d8245b1b4715c611b530a
Artem <artembogomaz@gmail.com>
2017-05-02 18:36:59 +0300

    dissable web-link preview in chat screen

--- Telegraph/TGModernConversationController.mm
+++ Telegraph/TGModernConversationController.mm
@@ -6204,7 +6204,7 @@ typedef enum {
     
     if (!TGStringCompare(_currentLinkParseLink, link))
     {
-        _disableLinkPreviewsForMessage = false;
+        _disableLinkPreviewsForMessage = true;
         if (link.length == 0)
         {
             _currentLinkParseLink = link;
@@ -6214,6 +6214,7 @@ typedef enum {
             {
                 [_inputTextPanel setSecondaryExtendedPanel:nil animated:true];
             }
+            
         }
         else
         {
@@ -6244,27 +6245,28 @@ typedef enum {
                     }
                     else
                     {
-                        TGModernConversationWebPreviewInputPanel *panel = nil;
-                        if ([[strongSelf->_inputTextPanel secondaryExtendedPanel] isKindOfClass:[TGModernConversationWebPreviewInputPanel class]])
-                        {
-                            panel = (TGModernConversationWebPreviewInputPanel *)[strongSelf->_inputTextPanel secondaryExtendedPanel];
-                        }
-                        else
-                        {
-                            panel = [[TGModernConversationWebPreviewInputPanel alloc] init];
-                            panel.dismiss = ^
-                            {
-                                __strong TGModernConversationController *strongSelf = weakSelf;
-                                if (strongSelf != nil)
-                                {
-                                    [strongSelf->_inputTextPanel setSecondaryExtendedPanel:nil animated:true];
-                                    strongSelf->_disableLinkPreviewsForMessage = true;
-                                }
-                            };
-                            [strongSelf->_inputTextPanel setSecondaryExtendedPanel:panel animated:true];
-                        }
                         
-                        [panel setLink:link webPage:webPage];
+                        //TGModernConversationWebPreviewInputPanel *panel = nil;
+                        //if ([[strongSelf->_inputTextPanel secondaryExtendedPanel] isKindOfClass:[TGModernConversationWebPreviewInputPanel class]])
+                        //{
+                        //    panel = (TGModernConversationWebPreviewInputPanel *)[strongSelf->_inputTextPanel secondaryExtendedPanel];
+                        //}
+                        //else
+                        //{
+                        //    panel = [[TGModernConversationWebPreviewInputPanel alloc] init];
+                        //    panel.dismiss = ^
+                         //   {
+                         //       __strong TGModernConversationController *strongSelf = weakSelf;
+                         //       if (strongSelf != nil)
+                         //       {
+                         //           [strongSelf->_inputTextPanel setSecondaryExtendedPanel:nil animated:true];
+                         //           strongSelf->_disableLinkPreviewsForMessage = true;
+                          //      }
+                          //  };
+                         //   [strongSelf->_inputTextPanel setSecondaryExtendedPanel:panel animated:true];
+                        //}
+                        
+                        //[panel setLink:link webPage:webPage];
                     }
                 }
             }]];
@@ -6365,7 +6367,7 @@ typedef enum {
         _disableLinkPreviewsForMessage = false;
     } else {
         [_companion controllerWantsToSendTextMessage:text entities:entities asReplyToMessageId:[self currentReplyMessageId] withAttachedMessages:[self currentForwardMessages] disableLinkPreviews:_disableLinkPreviewsForMessage botContextResult:nil botReplyMarkup:nil];
-        _disableLinkPreviewsForMessage = false;
+        _disableLinkPreviewsForMessage = true;
     }
 }
 
@@ -9442,18 +9444,22 @@ static UIView *_findBackArrow(UIView *view)
                         previewingContext.sourceRect = CGRectMake(location.x, location.y, 1.0f, 1.0f);
                         
                         if ([[link lowercaseString] hasPrefix:@"http://"] || [[link lowercaseString] hasPrefix:@"https://"] || [link rangeOfString:@"://"].location == NSNotFound) {
-                            NSURL *url = nil;
-                            @try {
-                                 url = [NSURL URLWithString:link];
-                            } @catch (NSException *e) {}
-                            if (url != nil && [[url.scheme lowercaseString] hasPrefix:@"http"]) {
-                                if (_inputTextPanel.maybeInputField.isFirstResponder)
-                                    _collectionViewIgnoresNextKeyboardHeightChange = true;
-                                
-                                SFSafariViewController *controller = [[SFSafariViewController alloc] initWithURL:url];
-                                return controller;
-                            }
+                            
+                            // removing preview for links
+                            //NSURL *url = nil;
+                            //@try {
+                            //    url = [NSURL URLWithString:link];
+                            //} @catch (NSException *e) {}
+                            //if (url != nil && [[url.scheme lowercaseString] hasPrefix:@"http"]) {
+                            //   if (_inputTextPanel.maybeInputField.isFirstResponder)
+                            //        _collectionViewIgnoresNextKeyboardHeightChange = true;
+                            //
+                            //    SFSafariViewController *controller = [[SFSafariViewController alloc] initWithURL:url];
+                             //   return controller;
+                            //}
+                            
                         }
+                        
                     }
                     else if ([(TGMessageViewModel *)item.viewModel isPreviewableAtPoint:[_collectionView convertPoint:collectionPoint toView:[cell contentViewForBinding]]])
                     {
